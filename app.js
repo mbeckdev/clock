d4284d073f963a8a3975bd5b7d241b9a5fb99172
mbeckdev <mbeckdev42@gmail.com>
2021-03-13 18:19:32 -0700

    Shortened hour hand

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function setDate() {
 
   const hours = now.getHours();
   const hoursDegrees = (hours / 12) * 360 + 90;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  hourHand.style.transform = `translateX(100%) rotate(${hoursDegrees}deg)`;
   // console.log(hours);
   // console.log(hoursDegrees);
 }
