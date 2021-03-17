b433234fe282ca35fae77dc455aad59fbb3d0b42
mbeckdev <mbeckdev42@gmail.com>
2021-03-16 19:47:40 -0600

    Fix hand jumping due to transitions

--- app.js
+++ app.js
@@ -13,13 +13,36 @@ function setDate() {
   const mins = now.getMinutes();
   const minsDegrees = (mins / 60) * 360 + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-  // console.log(mins);
 
   const hours = now.getHours();
   const hoursDegrees = (hours / 12) * 360 + 90;
   hourHand.style.transform = `translateX(100%) rotate(${hoursDegrees}deg)`;
-  // console.log(hours);
-  // console.log(hoursDegrees);
+
+  setTransitions(seconds, mins, hours);
+}
+
+function setTransitions(seconds, mins, hours) {
+  // transitions are set in JavaScript instead of CSS
+  //   so when the hand reaches 0, it doesn't transition all the way
+  //   around and look very unlike a clock.
+
+  if (seconds == 0) {
+    secondHand.style.transition = 'none';
+  } else {
+    secondHand.style.transition = 'all 0.05s';
+  }
+
+  if (mins == 0) {
+    minsHand.style.transition = 'none';
+  } else {
+    minsHand.style.transition = 'all 0.05s';
+  }
+
+  if (hours == 0) {
+    hourHand.style.transition = 'none';
+  } else {
+    hourHand.style.transition = 'all 0.05s';
+  }
 }
 
 setInterval(setDate, 1000);
